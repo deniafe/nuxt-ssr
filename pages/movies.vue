6d1a4f5d4dbc1ea8b3fa7a8e1a32854ba2551ff2
deniafe <deniafe@yahoo.com>
2021-04-23 15:03:13 +0100

    first commit with all codes

---
+++ pages/movies.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container mt-5">
+    <div class="row">
+      <div class="col-4 offset-4">
+        <div>
+          <input
+            v-model="name"
+            type="text"
+            class="form-control"
+            placeholder="What's your name?"
+            @input="onNameInput"
+          />
+        </div>
+        <h2 class="text-center mt-2">
+          {{ greeting }}
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      greeting: '',
+    }
+  },
+
+  methods: {
+    onNameInput() {
+      this.$axios.$get(`/api/hello/${this.name}`).then((response) => {
+        this.greeting = response.message
+      })
+    },
+  },
+}
+</script>
